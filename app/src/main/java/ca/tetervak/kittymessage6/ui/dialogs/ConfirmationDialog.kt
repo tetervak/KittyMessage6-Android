9c782e52aac3798b1c2f68bee75f0bbb4d636a80
Alex Tetervak <tetervak@sheridancollege.ca>
2020-10-11 23:05:10 -0400

    added confirm delete dialog

---
+++ app/src/main/java/ca/tetervak/kittymessage6/ui/dialogs/ConfirmationDialog.kt
@@ -0,0 +1,45 @@
+package ca.tetervak.kittymessage6.ui.dialogs
+
+import android.app.Activity
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+import ca.tetervak.kittymessage6.R
+
+class ConfirmationDialog : DialogFragment() {
+
+    companion object{
+
+        private const val MESSAGE = "message"
+        private const val TAG = "confirmation_dialog"
+
+        fun show(message: String, target: Fragment, requestCode: Int){
+            val fragment = ConfirmationDialog()
+            val arguments = Bundle()
+            arguments.putString(MESSAGE, message)
+            fragment.arguments = arguments
+            fragment.setTargetFragment(target, requestCode)
+            fragment.show(target.parentFragmentManager, TAG)
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        val activity = requireActivity()
+        val message = arguments?.getString(MESSAGE) ?: ""
+
+        return AlertDialog.Builder(activity).apply{
+            setTitle(R.string.app_name)
+            setMessage(message)
+            setPositiveButton(android.R.string.ok) { _, _ -> confirmed() }
+            setNegativeButton(android.R.string.cancel, null)
+        }.create()
+    }
+
+    private fun confirmed() {
+        targetFragment?.onActivityResult(targetRequestCode, Activity.RESULT_OK, null)
+    }
+
+}
--- app/src/main/java/ca/tetervak/kittymessage6/ui/output/OutputFragment.kt
+++ app/src/main/java/ca/tetervak/kittymessage6/ui/output/OutputFragment.kt
@@ -1,5 +1,7 @@
 package ca.tetervak.kittymessage6.ui.output
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
@@ -8,10 +10,15 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import ca.tetervak.kittymessage6.R
 import ca.tetervak.kittymessage6.databinding.FragmentOutputBinding
+import ca.tetervak.kittymessage6.ui.dialogs.ConfirmationDialog
 import ca.tetervak.kittymessage6.util.DateTimeStamp
 import java.util.*
 
 class OutputFragment : Fragment() {
+
+    companion object{
+        const val CONFIRM_DELETE: Int = 1
+    }
 
     private val safeArgs: OutputFragmentArgs by navArgs()
 
@@ -49,12 +56,25 @@ class OutputFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId){
             R.id.action_delete -> {
-                viewModel.delete()
-                showInput()
+                ConfirmationDialog.show(
+                    requireActivity()
+                        .getString(R.string.confirmation_message), this, CONFIRM_DELETE)
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == CONFIRM_DELETE && resultCode == Activity.RESULT_OK){
+            delete()
+        }
+    }
+
+    private fun delete(){
+        viewModel.delete()
+        showInput()
     }
 
     private fun showInput(){
