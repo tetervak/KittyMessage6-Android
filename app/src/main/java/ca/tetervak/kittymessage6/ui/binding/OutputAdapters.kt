d5e6ac06d59c23f04e1b6cc7af651a2baa066788
Alex Tetervak <tetervak@sheridancollege.ca>
2020-10-20 10:32:01 -0400

    removed the wrapper object in OutputAdapters.kt

--- app/src/main/java/ca/tetervak/kittymessage6/ui/binding/OutputAdapters.kt
+++ app/src/main/java/ca/tetervak/kittymessage6/ui/binding/OutputAdapters.kt
@@ -7,52 +7,45 @@ import ca.tetervak.kittymessage6.domain.CatMessage
 import ca.tetervak.kittymessage6.util.DateTimeStamp
 import java.util.*
 
-object OutputAdapters {
-
-    @BindingAdapter("app:date")
-    @JvmStatic
-    fun bindDate(textView: TextView, date: Date?){
-        if(date is Date)
-            textView.text = DateTimeStamp.formatDate(date)
-    }
-
-    @BindingAdapter("app:time")
-    @JvmStatic
-    fun bindTime(textView: TextView, date: Date?){
-        if(date is Date)
-            textView.text = DateTimeStamp.formatTime(date)
-    }
-
-    @BindingAdapter("app:date_time")
-    @JvmStatic
-    fun bindDateTime(textView: TextView, date: Date?){
-        if(date is Date)
-            textView.text = DateTimeStamp.formatDateTime(date)
-    }
-
-    @BindingAdapter("app:message")
-    @JvmStatic
-    fun bindCatMessage(textView: TextView, catMessage: CatMessage?){
-        if(catMessage is CatMessage) {
-            val catMessages = textView.resources.getStringArray(R.array.cat_messages)
-            textView.text = catMessages[catMessage.ordinal]
-        }
-    }
-
-    @BindingAdapter("app:delivery")
-    @JvmStatic
-    fun bindDelivery(textView: TextView, isUrgent: Boolean?){
-        if(isUrgent is Boolean){
-            val context = textView.context
-            textView.text =
-                if(isUrgent){
-                    context.getString(R.string.urgent)
-                }else{
-                    context.getString(R.string.not_urgent)
-                }
-        }
-    }
-
+@BindingAdapter("app:date")
+fun bindDate(textView: TextView, date: Date?) {
+    if (date is Date)
+        textView.text = DateTimeStamp.formatDate(date)
 }
+
+@BindingAdapter("app:time")
+fun bindTime(textView: TextView, date: Date?) {
+    if (date is Date)
+        textView.text = DateTimeStamp.formatTime(date)
+}
+
+@BindingAdapter("app:date_time")
+fun bindDateTime(textView: TextView, date: Date?) {
+    if (date is Date)
+        textView.text = DateTimeStamp.formatDateTime(date)
+}
+
+@BindingAdapter("app:message")
+fun bindCatMessage(textView: TextView, catMessage: CatMessage?) {
+    if (catMessage is CatMessage) {
+        val catMessages = textView.resources.getStringArray(R.array.cat_messages)
+        textView.text = catMessages[catMessage.ordinal]
+    }
+}
+
+@BindingAdapter("app:delivery")
+fun bindDelivery(textView: TextView, isUrgent: Boolean?) {
+    if (isUrgent is Boolean) {
+        val context = textView.context
+        textView.text =
+            if (isUrgent) {
+                context.getString(R.string.urgent)
+            } else {
+                context.getString(R.string.not_urgent)
+            }
+    }
+}
+
+
 
 
