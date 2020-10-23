980a82dbfb48d3823bd22030332166dd65660495
Alex Tetervak <tetervak@sheridancollege.ca>
2020-10-22 23:26:43 -0400

    renamed envelopeKey to envelopeId in the factory

--- app/src/main/java/ca/tetervak/kittymessage6/ui/output/OutputViewModelFactory.kt
+++ app/src/main/java/ca/tetervak/kittymessage6/ui/output/OutputViewModelFactory.kt
@@ -5,14 +5,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 class OutputViewModelFactory(
-    private val envelopeKey: Long,
+    private val envelopeId: Long,
     private val application: Application
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(OutputViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return OutputViewModel(envelopeKey, application) as T
+            return OutputViewModel(envelopeId, application) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
