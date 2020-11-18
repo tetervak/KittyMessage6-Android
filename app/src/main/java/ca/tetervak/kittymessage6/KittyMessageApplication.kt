456ee373e565ec09a281823e76962e3faffebc8e
Alex Tetervak <tetervak@sheridancollege.ca>
2020-11-17 19:44:12 -0500

    removed empty {} of the application class

--- app/src/main/java/ca/tetervak/kittymessage6/KittyMessageApplication.kt
+++ app/src/main/java/ca/tetervak/kittymessage6/KittyMessageApplication.kt
@@ -4,5 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class KittyMessageApplication: Application() {
-}
+class KittyMessageApplication: Application()
