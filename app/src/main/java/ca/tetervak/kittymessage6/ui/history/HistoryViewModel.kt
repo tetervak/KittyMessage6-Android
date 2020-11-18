c8cd37e2e57bcd92e60e9b30d2a6c9c44cf5a422
Alex Tetervak <tetervak@sheridancollege.ca>
2020-11-17 20:25:25 -0500

    removed unused imports

--- app/src/main/java/ca/tetervak/kittymessage6/ui/history/HistoryViewModel.kt
+++ app/src/main/java/ca/tetervak/kittymessage6/ui/history/HistoryViewModel.kt
@@ -1,9 +1,7 @@
 package ca.tetervak.kittymessage6.ui.history
 
-import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import ca.tetervak.kittymessage6.domain.Envelope
